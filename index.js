931baab35b0903839635f5f749534569e751f5b9
Alberto <42894888+Nenino2@users.noreply.github.com>
2022-04-27 21:32:57 +0200

    [main] Fix

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 "use strict";
 
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.dummyValue = "";
 
 const dummyObj = {}
-
 const dummyProxy = new Proxy(dummyObj, {
     get() {
         return dummyProxy;
     }
 })
 
-exports = dummyProxy;
+exports.default = dummyProxy;
